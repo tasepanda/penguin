443fd02ffa5ca380dac8573c5bf2fca7023e4e7c
{Keisuke <{k.tare.bee21@gmail.com}>
2021-06-05 20:00:57 +0900

    safemode

--- penguin_front/penguin.js
+++ penguin_front/penguin.js
@@ -1,13 +1,27 @@
-const button1 = document.getElementById("titleSubmit");
-const refresh = document.getElementById("answerSubmit");
+const refresh = document.getElementById("viewBtn");
+const titleBtn = document.getElementById("titleSubmit");
+const answerBtn = document.getElementById("answerSubmit");
+const table1 = document.getElementById("targetTable");
 
-button.addEventListener("click", async function(){
+titleBtn.addEventListener("click", async function(){
+    // APIをcallする
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await res.json();
+    console.log(users);
 
+    // DOM操作
     users.forEach(function(user){
-        const list = document.createElement("th");
+        // 行を追加する
+        let newRow = table1.insertRow();
+        // IDを入れる
+        let newCell = newRow.insertCell();
+        let list = document.createElement("th");
+        list.innerText = user.id;
+        newCell.appendChild(list);
+        // 議題を入れる
+        newCell = newRow.insertCell();
+        list = document.createElement("th");
         list.innerText = user.name;
-        lists.appendChild(list);
+        newCell.appendChild(list);
     })
 });
