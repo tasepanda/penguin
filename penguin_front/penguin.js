29dd8c6745d371fab9103b13b80c241ac32b4104
{Keisuke <{k.tare.bee21@gmail.com}>
2021-06-05 21:08:05 +0900

    0605_End

--- penguin_front/penguin.js
+++ penguin_front/penguin.js
@@ -1,10 +1,13 @@
 const refresh = document.getElementById("viewBtn");
 const titleBtn = document.getElementById("titleSubmit");
 const answerBtn = document.getElementById("answerSubmit");
-const table1 = document.getElementById("targetTable");
+const table1 = document.getElementById("titleTable");
+const table2 = document.getElementById("answerTable");
 
+// 議題の更新
 titleBtn.addEventListener("click", async function(){
     // APIをcallする
+    // URLの部分を更新すれば、画面が更新されるはず！
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await res.json();
     console.log(users);
@@ -20,8 +23,32 @@ titleBtn.addEventListener("click", async function(){
         newCell.appendChild(list);
         // 議題を入れる
         newCell = newRow.insertCell();
-        list = document.createElement("th");
+        list = document.createElement("td");
         list.innerText = user.name;
+        newCell.appendChild(list);
+    })
+});
+
+// 回答の更新
+answerBtn.addEventListener("click", async function(){
+    // APIをcallする
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+    console.log(users);
+
+    // DOM操作
+    users.forEach(function(user){
+        // 行を追加する
+        let newRow = table2.insertRow();
+        // IDを入れる
+        let newCell = newRow.insertCell();
+        let list = document.createElement("th");
+        list.innerText = user.id;
+        newCell.appendChild(list);
+        // 議題を入れる
+        newCell = newRow.insertCell();
+        list = document.createElement("td");
+        list.innerText = user.email;
         newCell.appendChild(list);
     })
 });
